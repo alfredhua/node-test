e48bd0e3a48f51633d57a79e16d705bd4453f0c6
huazhenguo <hua_zhenguo@163.com>
2021-07-16 23:48:24 +0800

    修改

--- rui-loan/investLoan.js
+++ rui-loan/investLoan.js
@@ -2,28 +2,16 @@
 const puppeteer = require('puppeteer');
 
 const 
-<<<<<<< HEAD
-url="http://m.railcapital.com.cn/offlineloan/confirm?id=26e1f784d53b45b1b6a70def3137ac72",
-domain=".railcapital.com.cn",
-rsessionid="b43c114cc04943aa8e116c3d4b93c28c",
-rsessionid_sig="znH-x1mfqXlYQiPIfa1kiaMSpfA",
-amount="150000";
-=======
 url="http://m.railcapital.com.cn/offlineloan/confirm?id=31c7268656b749139fb9252427aeae50",
 domain=".railcapital.com.cn",
 rsessionid="5771aec18fbf4be9b84f370489ab582c",
 rsessionid_sig="oWd52lO60Yzuu0Zco2ZhywmZ5yY",
 amount="100000";
->>>>>>> 7cfd0f98fac3b56b184227c44f9b34176bd61929
 
 (async()=>{
     console.log("------开始抢标-----")
     for(var i=1;i<10000;i++){
-<<<<<<< HEAD
-        const browser = await puppeteer.launch({headless:true});
-=======
         const browser = await puppeteer.launch({headless:false,devtools:true});
->>>>>>> 7cfd0f98fac3b56b184227c44f9b34176bd61929
         const page = await browser.newPage();
         await page.setCookie(
             {"name":"rsessionid","value":rsessionid,"domain":domain},
