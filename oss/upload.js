db28a76611053c7cc5f31c7c89fcd979a8118eec
huazhenguo <hua_zhenguo@163.com>
2021-08-23 07:25:08 +0800

    .gitignore

---
+++ oss/upload.js
@@ -0,0 +1,12 @@
+
+let OSS = require('ali-oss');
+
+let client = new OSS({
+  region: '<Your region>',
+  accessKeyId: '<Your AccessKeyId>',
+  accessKeySecret: '<Your AccessKeySecret>'
+});
+    
+(async()=>{
+
+})();
