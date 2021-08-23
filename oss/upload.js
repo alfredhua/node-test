b595bf77956637196451af01fb7eda00548a14d8
guozhenhua7 <guozhenhua7@jd.com>
2021-08-23 17:42:14 +0800

    oss

--- oss/upload.js
+++ oss/upload.js
@@ -3,18 +3,46 @@ let OSS = require('ali-oss');
 const data=require('./config.dev.json');
 var fs=require('fs');
 var request = require('request');
+const readline = require('readline');
 
 let client = new OSS({
   region: data.region,
   accessKeyId: data.accessKeyId,
-  accessKeySecret: data.accessKeySecret
+  accessKeySecret: data.accessKeySecret,
+  bucket:data.bucketName
 });
 
 
 (async()=>{
-    request("https://shop-1256119282.file.myqcloud.com/tooools/static/img/index/tool/timestamp-bf543ed911.png")
-        .pipe(fs.createWriteStream("./a.png")).on('close',function(){
-        console.log('pic saved!')
-   });
-    let result = await client.put('exampleobject.txt', 'D:\\localpath\\examplefile.txt');
+
+  // const result=await client.put('a.png','./test/redhat-0b738f24ab.png');
+  // console.log(JSON.stringify(result))
+
+  const fileStream = fs.createReadStream('./file/test.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  for await (const line of rl) {
+    let l=line.split("|");
+    if(l[1]!=''){
+      var fileName=l[1].substring(l[1].lastIndexOf("\/")+1,l[1].length);
+      const req =new Promise((resolve, reject) =>{
+          request(l[1])
+          .pipe(fs.createWriteStream("./test/"+fileName))
+          .on('close',async function(){ });
+      })
+      const result=await client.put("/test/"+fileName,"./test/"+fileName);
+      l[1]=result.url;
+      console.log(l[1])
+      fs.appendFile('./test2.txt',l.toString,function(error){
+          console.log("---------------")
+      });
+         
+     }
+    console.log(`Line from file: ${line}`);
+  }
+ 
 })();
