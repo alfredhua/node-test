de98b92dc1ec792a4640ca079d86437a25ad6749
huazhenguo <hua_zhenguo@163.com>
2021-08-23 08:08:33 +0800

    upload

--- oss/upload.js
+++ oss/upload.js
@@ -1,12 +1,20 @@
 
 let OSS = require('ali-oss');
+const data=require('./config.dev.json');
+var fs=require('fs');
+var request = require('request');
 
 let client = new OSS({
-  region: '<Your region>',
-  accessKeyId: '<Your AccessKeyId>',
-  accessKeySecret: '<Your AccessKeySecret>'
+  region: data.region,
+  accessKeyId: data.accessKeyId,
+  accessKeySecret: data.accessKeySecret
 });
-    
-(async()=>{
 
+
+(async()=>{
+    request("https://shop-1256119282.file.myqcloud.com/tooools/static/img/index/tool/timestamp-bf543ed911.png")
+        .pipe(fs.createWriteStream("./a.png")).on('close',function(){
+        console.log('pic saved!')
+   });
+    let result = await client.put('exampleobject.txt', 'D:\\localpath\\examplefile.txt');
 })();
